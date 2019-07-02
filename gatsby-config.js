a7cee26779bf90fc4278964531f3142b4d4c1011
Aranha Agency <aranhaagency@gmail.com>
2019-07-02 13:23:28 -0300

    start development

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `ARANHA`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@aranhaagency`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -26,9 +26,11 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    },
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  pathPrefix: "http://aranhaagency.github.io/aranhaagency.github.io/",
 }
+  
