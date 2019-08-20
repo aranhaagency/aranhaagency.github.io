ee53cf87954f8a27ad35530306b4b51617cabb63
Aranha Agency <aranhaagency@gmail.com>
2019-08-20 19:36:21 -0300

    update pathprefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -45,12 +45,10 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/favicon.png`, // This path is relative to the root of the site.
-      }, 
-      pathPrefix: "http://aranhaagency.github.io/aranhaagency.github.io",
+      }
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ]
 }
-  
